f5f4ca763572893b9df1d5db6c657145d31d3f5a
snowflaxgit <android@snowflax.com>
2013-06-15 13:01:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -18,7 +17,7 @@ var Server = mongo.Server,
     BSON = mongo.BSONPure;
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3560);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
